933b01d28d8cb8df437e560ebad11a65abf47f58
Poloju Ganesh <166226925+PolojuGanesh@users.noreply.github.com>
2025-09-03 08:31:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -176,7 +176,7 @@ app.delete("/api/addresses/:addressId", (req, res) => {
 });
 
 /* ---------------- SERVER ---------------- */
-const PORT = 5000;
+const PORT = 4000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
